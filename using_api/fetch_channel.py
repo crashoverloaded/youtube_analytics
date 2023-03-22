2fa58e4bddfee13182bc8d7d41ce665212d29208
crashoverloaded <2017pgicspriyank033@poornima.org>
2023-03-22 17:12:42 +0530

    Fetching video IDs

---
+++ using_api/fetch_channel.py
@@ -0,0 +1,17 @@
+import googleapiclient.discovery
+import requests
+# API information
+api_service_name = "youtube"
+api_version = "v3"
+keyFile = open('config.py', 'r')
+DEVELOPER_KEY = keyFile.readlines()[0].rstrip().split("=")[1]
+
+youtube = googleapiclient.discovery.build('youtube','v3',developerKey=DEVELOPER_KEY)
+id1 = "UCNU_lfiiWBdtULKOw6X0Dig"
+id2 = "UUNU_lfiiWBdtULKOw6X0Dig"
+#res = youtube.channels().list(id=id1,part="contentDetails").execute()
+r = youtube.playlistItems().list(playlistId=id2,part='snippet',maxResults=50).execute()
+for i in r['items']:
+    print(i['snippet'])
+
+print(r)
--- using_api/main.py
+++ using_api/main.py
@@ -1,22 +1,15 @@
 # API client library
 import googleapiclient.discovery
+import requests
 # API information
 api_service_name = "youtube"
 api_version = "v3"
 keyFile = open('config.py', 'r')
 DEVELOPER_KEY = keyFile.readlines()[0].rstrip().split("=")[1]
 # API client
-youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey = DEVELOPER_KEY)
-# Request body
-request = youtube.search().list(
-        part="id,snippet",
-        type='video',
-        q="Spider-Man",
-        videoDuration='short',
-        videoDefinition='high',
-        maxResults=1
-)
+youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey = DEVELOPER_KEY)
+res = requests.get("https://www.googleapis.com/youtube/v3/channels?part=snippet,contentDetails,statistics&forUsername={id}&key={api}".format(api=DEVELOPER_KEY,id='krishnaik06'))
 # Request execution
-response = request.execute()
+response = res.json()
 print(response)
+
